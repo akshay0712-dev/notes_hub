693d8d3783f117863c05121dd2ecfa4abc5e310e
akshay_dev <akshayrishu4@gmail.com>
2026-07-20 07:17:52 +0530

    fix

--- Backend/src/utils/supabase.js
+++ Backend/src/utils/supabase.js
@@ -1,4 +1,4 @@
-import { supabase } from "../config/supabase.js";
+import { supabase } from "../config/supabase";
 import fs from "fs";
 
 const uploadOnSupabase = async (localFilePath, fileName, contentType) => {
